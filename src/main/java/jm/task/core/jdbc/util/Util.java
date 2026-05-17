b91a5515259b781e1bfee6ca6237f3dd2ac75e81
KoyanishkaKrasotka <albiak350@gmail.com>
2026-05-17 15:46:18 +0500

    changed application.properties and Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,7 +4,6 @@ package jm.task.core.jdbc.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
