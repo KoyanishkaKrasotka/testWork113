992d85cb7858c343278371850d4f818595b1d349
KoyanishkaKrasotka <albiak350@gmail.com>
2026-05-17 15:44:52 +0500

    changed application.properties and Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,23 +1,33 @@
 package jm.task.core.jdbc.util;
 
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/testwork?useSSL=false";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
-
+    public static void main(String[] args) {
+        getConnection();
+    }
 
     public static Connection getConnection() {
+        Properties properties = new Properties();
         try {
+            InputStream inputStream = Util.class.getClassLoader().getResourceAsStream("application.properties");
+            properties.load(inputStream);
+            String URL = properties.getProperty("db.url");
+            String USERNAME = properties.getProperty("db.username");
+            String PASSWORD = properties.getProperty("db.password");
             return DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
             throw new RuntimeException("Не подключилось", e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
